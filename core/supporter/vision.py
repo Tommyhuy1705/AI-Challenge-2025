c6c19c3c3e4ea73c28c3b8f44bfc6cca4166f95d
KhanhGiauTen <nqkhanh2925@gmail.com>
2025-08-23 19:18:34 +0700

    feat(vision) : Build detect object from image class, add init, load, release method

--- core/supporter/vision.py
+++ core/supporter/vision.py
@@ -4,3 +4,28 @@ def count_objects(tracker: list[dict], target: str) -> int: #thêm class chứa 
 def classify_action(clip: list[str]) -> str:
     # Gọi mô hình phân loại hành động
     return "đang chạy"
+
+import torch
+import torchvision
+from PIL import Image
+from torchvision import transforms
+
+
+class detectObjectImage:
+    def __init__(self):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model = None
+        self.load()
+        
+    def load(self):
+        if self.model is not None:
+            return  
+        try:
+            self.model = 
+        except Exception as e:
+            raise RuntimeError(f"Model loading failed: {e}")
+        
+    def release(self):
+        self.model = None
+        torch.cuda.empty_cache()    
+    
