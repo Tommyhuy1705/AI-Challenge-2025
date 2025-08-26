b27223654c916b58cd15f979cc6cd6a46a73ad32
Dong <duongquangdong1008@gml.com>
2025-08-27 00:42:38 +0700

    Finish issue 18

--- core/engine/search_engine.py
+++ core/engine/search_engine.py
@@ -1,5 +1,7 @@
-from core.supporter.helper import getKeyword, searchByKeyword 
-def search(query: str) -> dict:
+from core.supporter.helper import calculate_similarity
+# from core.db_controller.crud import convert_to_vector, query_database
+
+def search(keyword: str) -> dict:
     """
     Purpose: tìm vật thể X được nhắc đến trong query.
 
@@ -12,68 +14,14 @@ def search(query: str) -> dict:
               "video2": path2.mp4,
             }
 
-    Pipelines: Lấy keyword từ query -> chuyển đổi thành vector -> tìm kiếm trong database -> trả về các video paths -> output JSON
+    Pipelines: Lấy keyword từ query -> lấy video dựa theo keyword trong database -> trả về các video paths -> output JSON
     """
     
-    target = getKeyword(query)
-    results = converttovector(target)
-    paths = [r for r in results]
-
+    _, paths = get_all_frames_by_keyword(keyword)
     
     output = {
         f"video{i+1}": path
         for i, path in enumerate(paths)
-    }
-    
+    } # sửa lại định dạng output
 
     return output
-
-def search_by_image(path: str, top_k: int):
-    """
-    Purpose: Tìm kiếm những ảnh hoặc nội dung có độ tương đồng cao với ảnh đầu vào.
-
-    Circumtance: Khi người dùng đưa vào một ảnh và muốn biết các ảnh/frame/đoạn hội thoại tương tự.
-
-    Args:
-        path (str): Đường dẫn tới ảnh đầu vào cần so sánh.
-
-    Returns:
-        list[dict]: Danh sách top N ảnh tương đồng nhất, ví dụ:
-            [
-                {"path": "img_01.jpg", "similarity": 0.91},
-                {"path": "img_03.jpg", "similarity": 0.87},
-                ...
-            ]
-
-    Pipelines:
-        Trích xuất vector đặc trưng từ ảnh đầu vào -> Tìm kiếm ảnh trong cơ sở dữ liệu theo vector gần nhất ->
-        Tính toán độ tương đồng -> Trả về danh sách top N ảnh gần nhất.
-    """
-
-    vector = getVector(path)
-    if vector is None:
-        return {"error": "Không tìm thấy vector tương ứng với ảnh."}
-
-    keyword = getKeywordByVector(vector)
-    if keyword is None:
-        return {"error": "Không tìm thấy từ khóa tương ứng với vector."}
-    
-    imgs = searchByKeyword(keyword)
-
-    if imgs is None:
-        return {"error": "Không tìm thấy ảnh tương tự."}
-    
-    similarities = []
-
-    for img in imgs:
-        img_vector = getVector(img)
-
-        sim = calSimilarity(vector, img_vector)
-        similarities.append({
-            "path": img,
-            "similarity": sim
-        })
-
-
-        top_similarity = sorted(similarities, key=lambda x: x['similarity'], reverse=True)[:top_k]
-    return top_similarity
--- core/supporter/helper.py
+++ core/supporter/helper.py
@@ -1,5 +1,2 @@
-def get_keywords(query: str) -> list[str]:
-    return ["keyword1", "keyword2", "keyword3"]  # Ví dụ từ khóa
-
 def calculate_similarity(vector1: list[float], vector2: list[float]) -> float:
     return 0.8 # Ví dụ độ tương đồng
