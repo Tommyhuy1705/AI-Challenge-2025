d9d216745a34ad15922b82261d0b555daf517641
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2025-08-27 15:51:02 +0700

    feat(search_flow): done funtion

--- core/engine/search_flow.py
+++ core/engine/search_flow.py
@@ -1,7 +1,10 @@
+from dotenv import load_dotenv
+import os
 from core.boostrap import ModelRegistry
-from core.db_controller.crud
+from core.db_controller.crud import get_all_frames_by_vector
+from core.db_controller.connect import connect_db
 
-def find_flow(state: list[str]):
+def find_flow(state: list[str], limit: int = 5):
     """
     It searches and identifies the exact frames corresponding to a sequence of key states within an action. 
     The function maps each state to its matching frame in the correct temporal order. 
@@ -14,9 +17,28 @@ def find_flow(state: list[str]):
 
     state_vectors = [clip_model.encode_text(s) for s in state]
 
-    #Search
-    for i, vec in enumerate(state_vectors):
-        # Find the most similar frame in the video for each state vector
-        # frames = 
-        # fottmat dict id,path
-        pass
+    #Connect
+    url = os.getenv("WEAVIATE_URL")
+    key = os.getenv("WEAVIATE_API_KEY")
+    client = connect_db(url, key)
+
+    results = []
+
+    # Search
+    for i, (s, vec) in enumerate(zip(state, state_vectors)):
+        frames = get_all_frames_by_vector(vector=vec, limit=limit)
+
+        frame_list = []
+        for obj in frames.objects:
+            props = obj.properties
+            frame_list.append({
+                "keyframeId": props.get("keyframeId"),
+                "keyframePath": props.get("keyframePath")
+            })
+
+        results.append({
+            "state": s,
+            "frames": frame_list
+        })
+
+    return results
