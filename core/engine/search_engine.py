670421f087cf3990839fbf3f5a3ed6a0ba58f293
Dong <duongquangdong1008@gml.com>
2025-08-28 00:16:32 +0700

    Update search engine

--- core/engine/search_engine.py
+++ core/engine/search_engine.py
@@ -1,7 +1,9 @@
-from core.supporter.helper import calculate_similarity
-# from core.db_controller.crud import convert_to_vector, query_database
+import os
+from core.boostrap import ModelRegistry
+from core.db_controller.crud import get_all_frames_by_vector
+from core.db_controller.connect import connect_db
 
-def search(keyword: str) -> dict:
+def search(keyword: list[str], limit: int = 5) -> dict:
     """
     Purpose: tìm vật thể X được nhắc đến trong query.
 
@@ -16,12 +18,51 @@ def search(keyword: str) -> dict:
 
     Pipelines: Lấy keyword từ query -> lấy video dựa theo keyword trong database -> trả về các video paths -> output JSON
     """
-    
-    _, paths = get_all_frames_by_keyword(keyword)
-    
-    output = {
-        f"video{i+1}": path
-        for i, path in enumerate(paths)
-    } # sửa lại định dạng output
+    clip_model = ModelRegistry.clip_model
+    if not clip_model:
+        raise ValueError("CLIP model is not loaded")
 
-    return output
+    keyw_vectors = [clip_model.encode_text(k) for k in keyword]
+
+    #Connect
+    url = os.getenv("WEAVIATE_URL")
+    key = os.getenv("WEAVIATE_API_KEY")
+    client = connect_db(url, key)
+    
+    results = []
+    temp_results = None
+    init_limit = limit * 20
+
+    # Search: Có filter từng phần => Không dùng code bên dưới nữa
+    for i, (k, vec) in enumerate(zip(keyword, keyw_vectors)):
+        frames = get_all_frames_by_vector(client, vector=vec, limit=init_limit)
+
+        frame_list = []
+        for obj in frames.objects:
+            props = obj.properties
+            frame_list.append({
+                "keyframeId": props.get("keyframeId"),
+                "keyframePath": props.get("keyframePath")
+            })
+
+        if temp_results is None:
+            temp_results = frame_list
+        else:
+            # Lấy giao với kết quả trước => Nếu không có code này thì kết quả sẽ là của từ khóa cuối cùng
+            prev_ids = {f["keyframeId"] for f in results}
+            temp_results = [f for f in frame_list if f["keyframeId"] in prev_ids]
+
+        # Giảm dần kết quả (ví dụ giữ lại 80%)
+        step_limit = max(limit, int(len(temp_results) * 0.8))
+        temp_results = temp_results[:step_limit]
+
+        results.append({
+            f"key{i}": k,
+            "frames": temp_results
+        })
+
+        # Nếu số lượng đã ≤ final_limit thì dừng sớm
+        if len(temp_results) <= limit:
+            break
+
+    return results
