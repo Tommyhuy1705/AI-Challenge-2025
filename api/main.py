ffcb7b420a10f069fb93e5a9c8ffcf346bf61114
Nhựt Nguyễn <nguyenminhnhut3607@gmail.com>
2025-08-28 09:00:14 +0700

    run static model

--- api/main.py
+++ api/main.py
@@ -2,17 +2,18 @@ import os
 from dotenv import load_dotenv
 import sys
 from flask import Flask
-from routes.chat import chat_bp
-from routes.search import search_bp
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from core.models.clip_model import CLIPModel
+from core import boostrap
+from routes.search import search_bp
 load_dotenv()
 
 app = Flask(__name__)
 
 # Register blueprints
-app.register_blueprint(chat_bp, url_prefix='/api/chat')
+# app.register_blueprint(chat_bp, url_prefix='/api/chat')
 app.register_blueprint(search_bp, url_prefix='/api/search')
 
 if __name__ == '__main__':
+    boostrap.load_clip_model()
     app.run(debug=True, host='0.0.0.0', port=5000)
